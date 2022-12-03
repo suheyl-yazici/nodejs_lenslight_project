a5fc33c91c838ae327da61b12db47137000fd68a
suheyl-yazici <h.suheylyazici@gmail.com>
2022-12-03 18:55:24 +0300

    ana sayfa , genel düzenlemeler

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -52,9 +52,17 @@ const getAllPhotos = async (req, res) => {
 const getAPhoto = async (req, res) => {
   try {
     const photo = await Photo.findById({ _id: req.params.id }).populate("user");
+
+    let isOwner = false;
+    
+    if(res.locals.user) {
+      isOwner = photo.user.equals(res.locals.user._id)
+    }
+
     res.status(200).render("photo", {
       photo,
       link: "photos",
+      isOwner,
     });
   } catch (error) {
     res.status(500).json({
@@ -118,7 +126,6 @@ const updatePhoto = async (req, res) => {
     });
   }
 };
-
 
 
 export { createPhoto, getAllPhotos, getAPhoto,deletePhoto, updatePhoto };
